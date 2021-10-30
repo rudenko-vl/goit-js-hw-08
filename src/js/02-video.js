ed0dc5cb534c5464626370fb7f498b21add047e6
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2021-10-30 23:16:03 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframeReff = document.querySelector('iframe');
+const player = new Player(iframeReff);
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(event) {
+  const { seconds } = event;
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
 
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
