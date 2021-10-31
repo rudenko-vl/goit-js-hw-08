fe321dd4b0b54818399b80c70614bb6f54eb0ad9
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2021-10-31 11:44:43 +0200

    fix task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,19 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-const iframeReff = document.querySelector('iframe');
-const player = new Player(iframeReff);
-player.on('timeupdate', throttle(onPlay, 1000));
-function onPlay(event) {
-  const { seconds } = event;
-  localStorage.setItem('videoplayer-current-time', seconds);
-}
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay({seconds}) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+function setTime() {
+  const time = localStorage.getItem('videoplayer-current-time');
+  if (time) {
+    player.setCurrentTime(time);
+    localStorage.removeItem('videoplayer-current-time');
+  }
+};
+window.addEventListener('DOMContentLoaded', setTime);
